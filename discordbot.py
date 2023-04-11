aa54de82056e36ffe2c455739e1210e34eab5ec0
KimPolar <128953651+KimPolar@users.noreply.github.com>
2023-04-11 17:07:43 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -71,6 +71,17 @@ async def 계산(ctx,
              fx: Option(str, "계산식"),):
     await ctx.respond("입력한 식 : `" + str(fx) + "`\n답 : `" + str(float(eval(fx))) + "`")
 
+@bot.slash_command(description="리뷰 수로 기댓값 계산")
+async def 기댓값(ctx,
+              별5: Option(str, "별 다섯개 리뷰 수"),
+              별4: Option(str, "별 네개 리뷰 수"),
+              별3: Option(str, "별 세개 리뷰 수"),
+              별2: Option(str, "별 두개 리뷰 수"),
+              별1: Option(str, "별 한개 리뷰 수"),):
+    r_arr = [int(별5), int(별4), int(별3), int(별2), int(별1)]
+    ex_val = (r_arr[0]+1)/(sum(r_arr)+5)
+    await ctx.respond("그 곳에서 시켰을 때 5점의 만족도를 얻을 확률의 기댓값 : " + str(ex_val) + "%")
+
 @bot.slash_command(description="그래프")
 async def 그래프(ctx,
              fx: Option(str, "함수"),
